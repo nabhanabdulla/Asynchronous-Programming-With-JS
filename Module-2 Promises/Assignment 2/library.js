8e856455f8da8002f1d4e84b56757568225282c5
nabhanpv <nabhu123@gmail.com>
2018-11-30 15:17:46 +0530

    Add search by type and by price

---
+++ Module-2 Promises/Assignment 2/library.js
@@ -0,0 +1,158 @@
+'use strict';
+
+(function(window) {
+    function myLibrary() {
+        const catalog = createRandomCatalog(100);
+        
+
+
+
+        return {
+            searchProductById: searchProductById,
+            searchProductsByPrice: searchProductsByPrice,
+            searchProductsByType: searchProductsByType,
+            searchAllProducts: searchAllProducts
+        }
+
+        // Returns a promise containing products within a range 
+        // price - difference and price + difference
+        function searchProductsByPrice(price, difference) {
+            let productsByPrice;
+            
+            productsByPrice = new Promise(function(resolve, reject) {
+                let products = [];
+
+                if(!isFinite(price)) {
+                    reject("Invalid price: " + price);
+                }
+                else {
+                    console.log(price);
+                    setTimeout(function() {
+                        catalog.forEach(function(item) {
+                            if(Math.abs(price - item.price) < difference) {
+                                products.push(item);
+                            }
+                        })
+        
+                        if(products.length > 0) {
+                            resolve(products);
+                        }
+                        else {
+                            reject("No products in the range " + price + " +- " + difference);
+                        }
+                    }, 1000);
+                }
+                
+            })
+
+            return productsByPrice;
+        }
+
+        // Returns a promise containing products with their type attribute
+        // equal to the type given as parameter to the function
+        function searchProductsByType(type) {
+
+            let productsByType = new Promise(function(resolve, reject) {
+                let products = [];
+                let possibleTypes = ['Book', 'Clothing', 'Electronics', 'Food'];
+                let pattern = new RegExp(type, "i");
+                let validType;
+
+                possibleTypes.forEach(function(possibleType) {
+                    if(pattern.test(possibleType) === true) {
+                        validType = possibleType;
+                    }
+                })
+
+                if(validType === undefined) {
+                    reject("Invalid type: " + type);
+                }
+                else {
+                    setTimeout(function() {
+                        catalog.forEach(function(item) {
+                            if((item.type).toLowerCase() == validType.toLowerCase()) {
+                                products.push(item);
+                            }
+                        });
+
+                        resolve(products);
+                    }, 1000);
+                    
+                }
+            
+            })
+
+            return productsByType;
+        }
+
+        // Returns a promise the product with id as the 
+        // id given as parameter to the function
+        function searchProductById(id) {
+            let product;
+            let productById;
+
+            catalog.forEach(function(item) {
+                if(item.id == id) {
+                    product = item;
+                }
+            })
+
+            productById = new Promise(function(resolve, reject) {
+                setTimeout(function() {
+                    if(product !== undefined) {
+                        resolve(product);
+                    }
+                    else {
+                        console.log(id);
+                        reject("No product with id: " + id);
+                    }
+                }, 1000);
+            });
+
+            return productById;
+        }
+
+        // Returns a promise with all the products in the catalog
+        function searchAllProducts() {
+            let all_products = new Promise(function(resolve, reject) {
+                setTimeout(function() {
+                    if(catalog.length > 0) {
+                        resolve(catalog);
+                    }
+                    else {
+                        reject("Catalog empty.");
+                    }
+                }, 1000); 
+            });
+
+            return all_products;
+        }
+
+        // Create a random product 
+        function createRandomObject() {
+            let typeArray = ['Book', 'Clothing', 'Electronics', 'Food'];
+            let price = (Math.random() * 500).toFixed(2);
+            let type = typeArray[Math.floor(Math.random() * 4)];
+            
+            return {price: price, type: type};
+        }
+
+        // Create a catalog having the number of products in it
+        // equal to the parameter provided
+        function createRandomCatalog(num) {
+            let catalog = [];
+            for(let i = 0; i < num; i++) {
+                let item = createRandomObject();
+                let product = {id: i+1, price: item.price, type: item.type};
+                
+                catalog.push(product);
+            }
+            return catalog;
+        }
+
+    }
+
+    if(typeof(window.api) === 'undefined') {
+        window.api = myLibrary();
+    }
+})(window);
---
+++ Module-2 Promises/Assignment 2/productCatalog.js
@@ -0,0 +1,191 @@
+'use strict';
+
+eventListen();
+
+
+// Event Listeners
+function eventListen() {
+    document.addEventListener('DOMContentLoaded', getProducts);
+    document.addEventListener('click', examineProduct);
+    document.addEventListener('click', clearInputs);
+}
+
+// Functions
+function sortProductsByPrice(products) {
+    for(let i = 0; i < (products.length - 1) ; i++) {
+        for(let j = i + 1; j < products.length; j++) {
+            if (parseFloat(products[i].price) > parseFloat(products[j].price)) {
+                let temp = products[i];
+                products[i] = products[j];
+                products[j] = temp;
+                console.log(products[i], products[j]);
+            }
+        }
+    }
+    return products;
+}
+
+function sortProductsByTypePrice(products) {
+    for(let i = 0; i < (products.length - 1) ; i++) {
+        for(let j = i + 1; j < products.length; j++) {
+            if (products[i].type > products[j].type) {
+                let temp = products[i];
+                products[i] = products[j];
+                products[j] = temp;
+                console.log(products[i], products[j]);
+            }
+            else if(products[i].type == products[j].type) {
+                if (parseFloat(products[i].price) > parseFloat(products[j].price)) {
+                    let temp = products[i];
+                    products[i] = products[j];
+                    products[j] = temp;
+                    console.log(products[i], products[j]);
+                }
+            }
+        }
+    }
+    return products;
+}
+
+function clearInputs(e) {
+    if(e.target.type === 'text') {
+        return new Promise(function(resolve, reject) {
+            setTimeout(function() {
+                let inputs = document.querySelectorAll('input');
+                inputs.forEach(input => input.value = '');
+            }, 0);
+    
+            resolve('Inputs cleared');
+        })  
+    }
+    
+}
+
+function examineProduct(e) {
+    let id;
+    //e.preventDefault();
+    if(e.target.id.includes('btn-search-id')) {
+        id = document.querySelector('#input-search-id').value;
+        console.log(id, typeof id);
+        displayById(id);
+    }
+    else if(e.target.id.includes('btn-search-type')) {
+        let type = document.querySelector('#input-search-type').value;
+        displayByType(type);
+    }
+    else if(e.target.id.includes('btn-search-price')) {
+        let price = document.querySelector('#input-search-price').value;
+        displayByPrice(price);
+    }
+    else if((e.target.classList.value.includes('btn-examine'))) {
+        id = e.target.id.slice(4);
+        displayById(id);
+    } 
+    
+}
+
+function displayByPrice(price) {
+    clearExaminedProduct();
+
+    api.searchProductsByPrice(price, 50).then(function(resultArray) {
+        resultArray = sortProductsByTypePrice(resultArray, 'price');
+        populateTable('table-similar-products', resultArray);
+        document.body.scrollTop = 560;
+    }).catch(function(error) {
+        console.log("Error displaying by price: " + error);
+    });
+}
+
+function displayByType(type) {
+    clearExaminedProduct();
+
+    api.searchProductsByType(type).then(function(resultArray) {
+        resultArray = sortProductsByPrice(resultArray, 'price');
+        populateTable('table-similar-products', resultArray);
+        document.body.scrollTop = 560;
+    }).catch(function(error) {
+        console.log("Error displaying by type: " + type);
+    });
+}
+
+function displayById(id) {
+    clearExaminedProduct();
+
+    api.searchProductById(id).then(function(result) {
+            
+        return Promise.all([result, 
+                            api.searchProductsByType(result.type), 
+                            api.searchProductsByPrice(result.price, 50) ]);
+
+    }).then(function(res) {
+        let similarArray = getIntersection(res[1], res[2], res[0].id);
+
+        showExaminedProduct(res[0]);
+        populateTable('table-similar-products', similarArray);
+        document.body.scrollTop = 360;
+    }).catch(function(error) {
+        console.log("Error: " + error);
+    });
+}   
+
+
+function getIntersection(arr1, arr2, searchedId) {
+    let similarArray = [];
+
+    arr1.forEach(function(product1) {
+        arr2.forEach(function(product2) {
+            if((product1.id == product2.id) && (product1.id != searchedId)) {
+                similarArray.push(product1);
+            }
+        })
+    })
+
+    return similarArray;
+}
+
+function clearExaminedProduct() {
+    let examinedList = document.querySelector('#list-examined-product');  
+    let span_tags = examinedList.querySelectorAll('span');
+
+    span_tags[0].innerHTML = '';
+    span_tags[1].innerHTML = '';
+    span_tags[2].innerHTML = '';
+}
+
+function showExaminedProduct(product) {
+    let examinedList = document.querySelector('#list-examined-product');  
+    let span_tags = examinedList.querySelectorAll('span');
+
+    span_tags[0].innerHTML = product.id;
+    span_tags[1].innerHTML = product.price;
+    span_tags[2].innerHTML = product.type;
+
+}
+
+function getProducts() {
+    api.searchAllProducts().then(function(result) {
+        populateTable('table-all-products', result);
+    });
+}
+
+function populateTable(tableId, productArray) {
+    let table = document.querySelector(`#${tableId}`);
+    let html = `<thead>
+                    <th>ProductId</th>
+                    <th>Type</th>
+                    <th>Price</th>
+                    <th>Examine</th>
+                </thead>`
+    
+    productArray.forEach(function(product) {
+        html += `
+            <tr>
+                <td>${product.id}</td>
+                <td>${product.type}</td>
+                <td>${product.price}</td>
+                <td><button id='btn-${product.id}' class='btn btn-examine'>Examine</button></td>
+            </tr>`;
+    });
+
+    table.innerHTML = html;
+}
